a26e09fa9b0678e1da2eddb7071a6fba92b3c340
TashwillAndries <tashwilla27@gmail.com>
2021-09-29 08:38:17 +0200

    new updates

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,59 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { body, validationResult, check } = require("express-validator");
 
 router.get("/", (req, res) => {
   res.send("Get logged in user");
 });
 
-router.post("/", (req, res) => {
-  res.send("log in user");
-});
+router.post(
+  "/",
+  [
+    check("email", "please include a valid email").isEmail(),
+    check("password", "please enter a valid password").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "invalid crendentails" });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
 
+connectDB();
 app.get("/", (req, res) =>
   res.send({ msg: "Welcome to the contact keeper API.." })
 );
+
+//init middleware
+app.use(express.json({ extended: false }));
 
 // define routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contact"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
